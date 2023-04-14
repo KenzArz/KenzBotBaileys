3c6c2136aace33828d1d2861891f4596f8331856
KenzArz <kenzart05@gmail.com>
2023-04-14 14:21:43 +0700

    create message objek

--- bot.js
+++ bot.js
@@ -1,10 +1,6 @@
-import makeWASocket ,{ DisconnectReason,useMultiFileAuthState, fetchLatestBaileysVersion, downloadMediaMessage } from'@adiwajshing/baileys';
+import makeWASocket ,{ DisconnectReason,useMultiFileAuthState, fetchLatestBaileysVersion, downloadMediaMessagem, S_WHATSAPP_NET } from'@adiwajshing/baileys';
 import {Boom} from '@hapi/boom';
-import { writeFile } from 'fs/promises';
-import axios from 'axios';
-
-import {sendMsg} from './app.js';
-
+import {Message} from './message'
 
 export let client;
 
@@ -24,6 +20,7 @@ async function connecting () {
   
   client.ev.on ('creds.update', saveCreds)
 
+
   client.ev.on('connection.update', async update => {
       const { connection, lastDisconnect } = update
       if(connection === 'close') {
@@ -41,148 +38,21 @@ async function connecting () {
       const msg = m.message[0];
       if(!msg.message) return
       await client.readMessages([msg.key]);
-      const {pushName: contactName, message, key:{remoteJid: contact, fromMe: bot}, key} = msg, ctxInfo = message?.extendedTextMessage?.contextInfo
       
-      const getMsg = message?.extendedTextMessage?.text , quotedMsg = ctxInfo?.quotedMessage?.conversation || ctxInfo?.quotedMessage?.extendedTextMessage?.text, caption = message?.imageMessage?.caption, quotedCaption = ctxInfo?.quotedMessage?.imageMessage?.caption;
-      const conversation = getMsg || caption || quotedMsg || quotedCaption; 
 
-      if(!getMsg.startsWith('!', 0))return;
-      const message_objek = await createMessage(msg, conversation)
-      
-      const isGroup = contact.includes('@g.us'),
-      owner = process.env.OWNER_number
-            
+      const message_objek = new Message(msg)
+                  
       //cek command
-      processCommand(message_objek).catch(err => {
-        error()
-      })
+      if(message_objek.message.body.includes('!'))
+      {
+          await processCommand(message_objek).catch(err => {
+          await message_objek.message.reply(`*Terjadi Error*
 
-
-
-    })
-    // await connect.WA()
-}
-
-export async function createMessage(msg, body) {
-  const checkType = (type, msgDetail) => {
-    const arrType = [{
-      type: 'imageMessage',
-      typeMsg: 'image',
-      isMedia: true
-    },
-    {
-      type: 'videoMessage',
-      typeMsg: msgDetail.videoMessage.gifPlayback ? 'gif' : 'video',
-      isMedia: true
-    },
-    {
-      type: 'audioMessage',
-      typeMsg: msgDetail.audioMessage.ppt ? 'vn' : 'audio',
-      isMedia: true
-    },
-    {
-      type: 'documentMessage',
-      typeMsg: 'document',
-      isMedia: true
-    },
-    {
-      type: 'stickerMessage',
-      typeMsg: 'sticker',
-      isMedia: true
-    }
-  ]
-  return type(arrType)
-  }
-  const getMsgType = msg.ephemeralMessage?.message || msg.documentWithCaptionMessage.message || msg.message
-  const getType = checkType( findType => findType.find(findTypeMsg =>  Object.keys(getMsgType).find(keyType => keyType === findTypeMsg.type)))
-
-  const isMedia = getType.isMedia ? getType.typeMsg : null
-  const {pushName: contactName, key:{remoteJid: contact, fromMe: bot}, key} = msg
-
-  const message = {
-    contactID: msg.key,
-    contactName: msg.pushName,
-    body,
-    typeMsg: getType.typeMsg,
-    isMedia,
-    quotedMessage: () => {
-      msg.key.participant = msg?.extendedTextMessage?.contextInfo?.participant
-
-      const ctxInfo = msg.message?.extendedTextMessage?.contextInfo || msg.message?.imageMessage?.contextInfo || msg.message?.audioMessage?.contextInfo
-      const quoted = ctxInfo.quotedMessage
-      if(!quoted) return
-
-      const getType = checkType(findType => findType.find(findOBJ => Object.keys(quoted).find(m => m === findOBJ)))
-      const isMedia = getType.isMedia ? getType.typeMsg : null
-      
-      return {
-        quoted: {
-        key: msg.key,
-        message: quoted
-        },
-        typeMsg: getType.typeMsg,
-        isMedia,
-        media: isMedia ? async function media() {
-          const download = await downloadMedia(this.quoted);
-          writeFile(path, download);
-          
-        } : null,
-        imgUrl: isMedia ? async function imageUrl(url, path) {
-          const urlImage = await downloadMediaUrl(url)
-          writeFile(path, urlImage)
-        } : null,
-        reply: async function (text, options) {
-          options.quoted = this.quoted
-          await delayMsg(contact, text, options)
-        }
+          ${err.toString()}`)
+        })
       }
-    },
-    mentions: contact,
-    media: isMedia ? async function media() {
-      const download = await downloadMedia(this.quoted);
-      writeFile(path, download);
-      
-    } : null,
-    imgUrl: isMedia ? async function imageUrl(url, path) {
-      const urlImage = await downloadMediaUrl(url)
-      writeFile(path, urlImage)
-    } : null,
-    reply: async function (text, options) {
-      options.quoted = this.quoted
-      await delayMsg(contact, text, options)
-    }
-  }
 
 
-
- async function delayMsg(contact, body, options = {}) {
-    options.ephemeralExpiration = 60*60*24 
-    await client.presenceSubscribe(contact)
-    await delay(options.kuisDate || 500)
-
-    await client.sendPresenceUpdate('composing', contact)
-    await delay(options.kuisDate || 500)
-    
-    await client.sendPresenceUpdate('paused', contact)
-  
-    await client.sendMessage(contact, body, options)
-  }
-
-  async function downloadMedia(imageMessage) {
-    await downloadMediaMessage(imageMessage, 'buffer')
-  }
-  
-  async function downloadMediaUrl(url) {
-    const getUrl = await axios({url, responseType: 'stream'})
-    new Promise (async (resolve, reject) => {
-      await getUrl.data
-      .pipe(fs.createWriteStream(filePath))
-      .on('finish', () => resolve())
-      .on('error', e => reject(e));
     })
-  }
-
 }
-
-
 connecting()
