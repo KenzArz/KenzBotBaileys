7337725ca7f78fec21dfd440dab429cc7d04cb4e
KenzArz <kenzart05@gmail.com>
2023-06-10 17:59:55 +0700

    mempermudah script igdl

--- command/convert/igdl.js
+++ command/convert/igdl.js
@@ -1,5 +1,4 @@
-import got from 'got'
-import vm from 'vm'
+import { POST } from '../../system/scraper/savefrom'
 
 export default async function (msg) {
   const quotedMessage = await msg.quotedMessage()
@@ -8,47 +7,34 @@ export default async function (msg) {
   if(!url.includes('instagram.com')) return {text: 'link tidak valid pastikan link benar dari instagram', error: true}
 
   await msg.reaction('process')
-  const script = await got('https://worker.sf-tools.com/savefrom.php', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      origin: 'https://id.savefrom.net',
-      referer: 'https://id.savefrom.net/',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36'
-    },
-    form: {
-      sf_url: url,
-      sf_submit: '',
-      new: 2,
-      lang: 'id',
-      app: '',
-      country: 'id',
-      os: 'Windows',
-      browser: 'Chrome',
-      channel: ' main',
-      'sf-nomad': 1
-    }
-  })
-  const executeCode = '[]["filter"]["constructor"](b).call(a);'
-  const fileScript = script.body.replace(executeCode, `
-  try {const script = ${executeCode.split('.call')[0]}.toString();if (script.includes('function showResult')) scriptResult = script;else (${executeCode.replace(/;/, '')});} catch {}
- `)
+  const contents = await POST(url)
+  const isArray = Array.isArray(contents)
 
-  const context = {
-    scriptResult: '',
-    log: console.log
-  }
-  vm.createContext(context)
-  new vm.Script(fileScript).runInContext(context)
-  const data = context.scriptResult.split('window.parent.sf.videoResult.show(')?.[1] || context.scriptResult.split('window.parent.sf.videoResult.showRows(')?.[1]
-
-  const content = data.split(');')[0].split(",\"instagram.com\"")[0]
-  const toJSON = JSON.parse(content)
-  const isArray = Array.isArray(toJSON)
-  
   if(!isArray) {
-    let media = {}
-    for(const content of toJSON.url) {
+    const setting = setMedia(contents.url)
+    const fetching = await msg.urlDownload(contents.thumb)
+    const thumb = await msg.resize(fetching)
+    setting.jpegThumbnail = thumb
+    await msg.reply(msg.mentions, setting)
+    return msg.reaction({stop:true})
+  }
+
+  let index = ''
+  for(const [content] of contents.entries()) {
+    index+= `\n${content + 1}`
+  }
+  const contentMedia = await msg.reply(msg.mentions, {
+    text: `ketik angka yang sesuai slide dipostingan untuk didownload\n${index}\n\n*NOTE*\nbalas 99 untuk mendownload semua media`
+  }, {quoted: msg.quotedID})
+
+  const {tempStore} = await import('../../bot.js')
+  tempStore({message: contentMedia, contents, setMedia})
+  return msg.reaction('')
+}
+
+function setMedia(data) {
+  let media = {}
+    for(const content of data) {
       media.url = content.url
       media.type = content.type
      }
@@ -62,22 +48,5 @@ export default async function (msg) {
       message.video = {url: media.url}
       message.mimetype = 'video/mp4'
     }
-
-    const fetching = await msg.urlDownload(toJSON.thumb)
-    const thumb = await msg.resize(fetching)
-    message.jpegThumbnail = thumb
-    await msg.reply(msg.mentions, message)
-    return msg.reaction({stop:true})
-  }
-  let index = ''
-  for(const [content] of toJSON.entries()) {
-    index+= `\n${content + 1}`
-  }
-  const contentMedia = await msg.reply(msg.mentions, {
-    text: `ketik angka yang sesuai slide dipostingan untuk didownload\n${index}\n\n*NOTE*\nbalas 99 untuk mendownload semua media`
-  }, {quoted: msg.quotedID})
-
-  const {tempStore} = await import('../../bot.js')
-  tempStore({message: contentMedia, content: toJSON})
-  return msg.reaction('')
+    return message
 }
--- command/convert/igdlQuoted.js
+++ command/convert/igdlQuoted.js
@@ -1,57 +1,25 @@
-export default async function(msg, {content}) {
+export default async function(msg, {contents, setMedia}) {
   await msg.reaction({loading: true})
   const int = parseInt(msg.body)
   if(int == '99') {
-    for(const media of content){
-      const data = {}
-      for(const body of media.url) {
-        data.url = body.url
-        data.type = body.type
-      }
-
-      const message = {}
-      if(data.type == 'webp' || data.type == 'webp') {
-        message.image = {url: data.url}
-        message.mimetype = 'image/jpeg'
-      }
-      else if(data.type == 'mp4') {
-        message.video = {url: data.url}
-        message.mimetype = 'video/mp4'
-      }
-
+    for(const media of contents.url){
+      const setting = setMedia(media)
       const fetching = await msg.urlDownload(media.thumb)
       const thumb = await msg.resize(fetching)
-      message.jpegThumbnail = thumb
-
-      await msg.reply(msg.mentions, message)
+      setting.jpegThumbnail = thumb
+      await msg.reply(msg.mentions, setting)
     }
     await msg.reaction({stop: true})
     return msg.reaction('succes')
   }
 
-  const array = content[int - 1]
-
-  const data = {}
-  for(const body of array.url) {
-    data.url = body.url
-    data.type = body.type
-  }
-
-  const message = {}
-  if(data.type == 'webp' || data.type == 'webp') {
-    message.image = {url: data.url}
-    message.mimetype = 'image/jpeg'
-  }
-  else if(data.type == 'mp4') {
-    message.video = {url: data.url}
-    message.mimetype = 'video/mp4'
-  }
-
+  const array = contents[int - 1]
+  const setting = setMedia(array.url)
   const fetching = await msg.urlDownload(array.thumb)
   const thumb = await msg.resize(fetching)
-  message.jpegThumbnail = thumb
+  setting.jpegThumbnail = thumb
 
-  await msg.reply(msg.mentions, message)
+  await msg.reply(msg.mentions, setting)
   await msg.reaction({stop: true})
   return msg.reaction('succes')
 }
