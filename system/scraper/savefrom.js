99428bff226bada2a2c91439c8dff0745d1ddfb5
KenzArz <86587438+KenzArz@users.noreply.github.com>
2023-07-13 08:29:58 +0000

    menambahkan y2mate sebagai alternatif lain

--- system/scraper/savefrom.js
+++ system/scraper/savefrom.js
@@ -1,5 +1,6 @@
 import got from 'got'
 import vm from 'vm'
+import fetch from 'node-fetch'
 
 export async function POST(url) {
     const script = await got('https://worker.sf-tools.com/savefrom.php', {
@@ -23,7 +24,7 @@ export async function POST(url) {
       'sf-nomad': 1
     }
   })
-
+    if(!script.ok) return {error: 'to many request'}
     const executeCode = '[]["filter"]["constructor"](b).call(a);'
     const fileScript = script.body.replace(executeCode, `
     try {const script = ${executeCode.split('.call')[0]}.toString();if (script.includes('function showResult')) scriptResult = script;else (${executeCode.replace(/;/, '')});} catch {}
@@ -38,6 +39,25 @@ export async function POST(url) {
     new vm.Script(fileScript).runInContext(context)
     const data = context.scriptResult.split('window.parent.sf.videoResult.show(')?.[1] || context.scriptResult.split('window.parent.sf.videoResult.showRows(')?.[1]
 
+    if(!data) {
+      const fetching = await fetch('https://www.y2mate.com/mates/analyzeV2/ajax', {
+        headers: {'content-type': 'application/x-www-form-urlencoded'},
+        method: 'POST',
+        body: new URLSearchParams(Object.entries({
+          k_query: url,
+          k_page: 'home',
+          q_auto: 0,
+          hl: 'en'
+        }))
+      })
+      const content = await fetching.json()
+      const regex = /\[\d+x\d+\]/
+      if(content.mess) return {error: content.mess}
+      for(const lower of content.links?.video) {
+        lower.type = lower.q_text.toLowerCase().replace(regex, '').trim()
+      }
+      return {url: content.links?.video, thumb: content.thumbnail}
+    }
     const content = data.split(');')[0].split(",\"instagram.com\"")[0]
     return JSON.parse(content)
 }
