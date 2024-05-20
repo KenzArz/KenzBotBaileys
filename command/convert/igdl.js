88abb31dfc739e52371e569e6838b0a77c52d69b
KenzArz <kenzart05@gmail.com>
2024-05-20 15:00:09 +0700

    fix igdl

--- command/convert/igdl.js
+++ command/convert/igdl.js
@@ -1,72 +1,89 @@
-import { igdl } from '../../system/scraper/y2mate.js'
-import https from 'https'
-import { Create_message } from '../../system/message.js'
+import { igdl } from "../../system/scraper/y2mate.js";
+import https from "https";
+import { Create_message } from "../../system/message.js";
 
 /**@param {Create_message} msg */
 export default async function (msg) {
-  const quotedMessage = msg.quotedMessage()
-  const url = quotedMessage?.body || msg.body.split(' ')[1]
+	const quotedMessage = msg.quotedMessage();
+	const url = quotedMessage?.body || msg.body.split(" ")[1];
 
-  const regex = /(?:https:\/\/:?www.instagram.com)\/((:?reel\/|:?p\/)([-_0-9A-Za-z]{11}))|(stories\/(.+?)\/)([-_0-9A-Za-z]{19})/
-  if(!regex.test(url)) return {text: 'link tidak valid pastikan link benar dari instagram', error: true}
+	const regex =
+		/(?:https:\/\/:?www.instagram.com)\/((:?reel\/|:?p\/)([-_0-9A-Za-z]{11}))|(stories\/(.+?)\/)([-_0-9A-Za-z]{19})/;
+	if (!regex.test(url))
+		return {
+			text: "link tidak valid pastikan link benar dari instagram",
+			error: true,
+		};
 
-  await (url.includes('reel') ? msg.reaction({loading: true}) : msg.reaction('process'))
-  const contents = await igdl(url)
-  if(contents.status != 'ok')return{error: true, text: '*ERROR*\nsilahkan coba lagi!'}
-  
-  if(contents.link.length  <= 1) {
-    const size = {
-      width: 300,
-      height: 300
-    }
-    const setting = await setMedia(contents.link, msg.urlDownload)
-    const mediaBuffer = setting?.image || setting?.video
-    if(!mediaBuffer) {
-      await msg.reaction({stop: true})
-      return msg.reaction('succes')
-    }
-    const fetching = await msg.urlDownload(contents.thumbnail)
-    const validateThumb = fetching == "Forbidden" || fetching?.error || !fetching;
-    
-    let thumb;
-    if(validateThumb) thumb = await msg.resize('./system/image/Error_Thumbnail.png',size)
-    else {
-      thumb = await msg.resize(fetching,size)
-    }
-    setting.jpegThumbnail = thumb
-    
-    await msg.reply(msg.room_chat, setting)
-    await msg.reaction({stop: true})
-    return msg.reaction('succes')
-  }
+	await (url.includes("reel")
+		? msg.reaction({ loading: true })
+		: msg.reaction("process"));
+	const contents = await igdl(url);
+	if (contents.status != "ok")
+		return { error: true, text: "*ERROR*\nsilahkan coba lagi!" };
 
-  let index = ''
-  for(const [content] of contents.link.entries()) {
-    index+= `\n${content + 1}`
-  }
-  const contentMedia = await msg.reply(msg.room_chat, {
-    text: `ketik angka yang sesuai slide dipostingan untuk didownload\n${index}\n\n*NOTE*\nbalas 99 untuk mendownload semua media`
-  }, {quoted: msg.quotedID})
+	if (contents.link.length <= 1) {
+		const size = {
+			width: 300,
+			height: 300,
+		};
+		const setting = await setMedia(contents.link, msg.urlDownload);
+		const mediaBuffer = setting?.image || setting?.video;
+		if (!mediaBuffer) {
+			await msg.reaction({ stop: true });
+			return msg.reaction("failed");
+		}
+		const fetching = await msg.urlDownload(contents.thumbnail);
+		const validateThumb =
+			fetching == "Forbidden" || fetching?.error || !fetching;
 
-  const {tempStore} = await import('../../bot.js')
-  tempStore({message: contentMedia, contents, setMedia})
-  return msg.reaction('')
+		let thumb;
+		if (validateThumb)
+			thumb = await msg.resize("./system/image/Error_Thumbnail.png", size);
+		else {
+			thumb = await msg.resize(fetching, size);
+		}
+		setting.jpegThumbnail = thumb;
+
+		await msg.reply(msg.room_chat, setting);
+		await msg.reaction({ stop: true });
+		return msg.reaction("succes");
+	}
+
+	let index = "";
+	for (const [content] of contents.link.entries()) {
+		index += `\n${content + 1}`;
+	}
+	const contentMedia = await msg.reply(
+		msg.room_chat,
+		{
+			text: `ketik angka yang sesuai slide dipostingan untuk didownload\n${index}\n\n*NOTE*\nbalas 99 untuk mendownload semua media`,
+		},
+		{ quoted: msg.quotedID }
+	);
+
+	const { tempStore } = await import("../../bot.js");
+	tempStore({ message: contentMedia, contents, setMedia });
+	return msg.reaction("");
 }
 
 async function setMedia(data, dwd) {
-  const agent = new https.Agent({ keepAlive: true, timeout: 10000 });
-  let message = {}
-  
-  for(const content of data) {
-    const bufferMedia = await dwd(content.url, agent)
-    if(content.type == 'mp4') {
-      message.video = bufferMedia
-      message.mimetype = 'video/mp4'
-    }
-    else if(content.type == 'webp' || content.type == 'jpg' || content.type == "heic") {
-      message.image = bufferMedia
-      message.mimetype = 'image/jpeg'
-    }
-  }
-  return message
+	const agent = new https.Agent({ keepAlive: true, timeout: 10000 });
+	let message = {};
+
+	for (const content of data) {
+		const bufferMedia = await dwd(content.url, agent);
+		if (content.type == "mp4") {
+			message.video = bufferMedia;
+			message.mimetype = "video/mp4";
+		} else if (
+			content.type == "webp" ||
+			content.type == "jpg" ||
+			content.type == "heic"
+		) {
+			message.image = bufferMedia;
+			message.mimetype = "image/jpeg";
+		}
+	}
+	return message;
 }
--- system/scraper/y2mate.js
+++ system/scraper/y2mate.js
@@ -1,92 +1,116 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 
-export async function POST({url, formData, type, isConvert}) {
-  const baseUrl = 'https://www.y2mate.com/mates/'
-  const fetching = await fetch(baseUrl.concat(url), {
-      method: 'POST',
-      headers: {
-          accept: "*/*",
-          'accept-language': "en-US,en;q=0.9",
-          'content-type': "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-      body: new URLSearchParams(Object.entries(formData))
-  })
+export async function POST({ url, formData, type, isConvert }) {
+	const baseUrl = "https://www.y2mate.com/mates/";
+	const fetching = await fetch(baseUrl.concat(url), {
+		method: "POST",
+		headers: {
+			accept: "*/*",
+			"accept-language": "en-US,en;q=0.9",
+			"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+		},
+		body: new URLSearchParams(Object.entries(formData)),
+	});
 
-  const json = await fetching.json();
-  if(isConvert) {
-    return json.dlink
-  }
+	const json = await fetching.json();
+	if (isConvert) {
+		return json.dlink;
+	}
 
-  if(type == 'mp3') {
-    const mp3 = json.links.mp3.mp3128
-    return {
-      size: mp3.size,
-      bitrate: mp3.q,
-      id: mp3.k
-    }
-  }
-  else if(type == 'insta') {
-    return json
-  }
+	if (type == "mp3") {
+		const mp3 = json.links.mp3.mp3128;
+		return {
+			size: mp3.size,
+			bitrate: mp3.q,
+			id: mp3.k,
+		};
+	} else if (type == "insta") {
+		return json;
+	}
 
-  const metadata = []
-  const mp4 = json.links.mp4
-  for(const links in mp4) {
-    const size = mp4[links].size
-    const bitrate = mp4[links].q
-    const id = mp4[links].k
+	const metadata = [];
+	const mp4 = json.links.mp4;
+	for (const links in mp4) {
+		const size = mp4[links].size;
+		const bitrate = mp4[links].q;
+		const id = mp4[links].k;
 
-    metadata.push({size, bitrate, id})
-  }
-  const video = metadata.sort((a,b) => {
-    const parseIntA = parseInt(a.bitrate)
-    const parseIntB = parseInt(b.bitrate)
+		metadata.push({ size, bitrate, id });
+	}
+	const video = metadata.sort((a, b) => {
+		const parseIntA = parseInt(a.bitrate);
+		const parseIntB = parseInt(b.bitrate);
 
-    return parseIntA - parseIntB
-  })
-  
-  return {title: json.title, thumbnail: `https://i.ytimg.com/vi/${json.vid}/0.jpg`, video}
+		return parseIntA - parseIntB;
+	});
+
+	return {
+		title: json.title,
+		thumbnail: `https://i.ytimg.com/vi/${json.vid}/0.jpg`,
+		video,
+	};
 }
 
-export async function ytdl (link, type) {
-  const ytIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:shorts\/)|(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
-  if(!ytIdRegex.test(link))return {text: 'url tidak valid', error: true}
-  const ytId = ytIdRegex.exec(link)
-  const url = 'https://youtu.be/' + ytId[1]
+export async function ytdl(link, type) {
+	const ytIdRegex =
+		/(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:shorts\/)|(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
+	if (!ytIdRegex.test(link)) return { text: "url tidak valid", error: true };
+	const ytId = ytIdRegex.exec(link);
+	const url = "https://youtu.be/" + ytId[1];
 
-  const content = await POST({url: `analyzeV2/ajax`, formData: {
-    k_query: url,
-    q_auto: '0',
-    k_page: 'home',
-    hl: 'en'
-  }, type})
+	const content = await POST({
+		url: `analyzeV2/ajax`,
+		formData: {
+			k_query: url,
+			q_auto: "0",
+			k_page: "home",
+			hl: "en",
+		},
+		type,
+	});
 
-  return {
-    ID: { 
-      vid: ytId[1],
-    },
-    data: content
-  }
+	return {
+		ID: {
+			vid: ytId[1],
+		},
+		data: content,
+	};
 }
 
 export async function igdl(link) {
-  const media = await POST({url: `analyzeV2/ajax`, formData: {
-    k_query: link,
-    q_auto: '1',
-    k_page: 'instagram',
-    hl: 'en'
-  }, type: 'insta'})
-  
-  const contents = media.links.video.map((d, i) => {
-    const pattern = /JPG|JPEG|MP4||heic/gi
-    const type = d.q_text.match(pattern)[0].toLowerCase()
+	const media = await POST({
+		url: `analyzeV2/ajax`,
+		formData: {
+			k_query: link,
+			q_auto: "1",
+			k_page: "instagram",
+			hl: "en",
+		},
+		type: "insta",
+	});
 
-    return {...d,thumb: media.gallery?.items[i]?.thumb, type}
-  })
+	const contents = media.links.video.reduce((result, d, i) => {
+		const pattern = /(JPG|JPEG|MP4||HEIC)\s(.*)/i;
+		const text = d.q_text.match(pattern);
+		const [_qtext, type, bitrate] = text;
+		if (
+			!media.gallery &&
+			(bitrate.includes("1080") || bitrate.includes("video"))
+		)
+			result.push({ url: d.url, type: type.toLowerCase() });
+		else if (media.gallery) {
+			result.push({
+				url: d.url,
+				thumb: media.gallery?.items[i]?.thumb,
+				type: type.toLowerCase(),
+			});
+		}
+		return result;
+	}, []);
 
-  return {
-    status: media.status,
-    link: contents,
-    thumbnail: media.thumbnail
-  }
+	return {
+		status: media.status,
+		link: contents,
+		thumbnail: media.thumbnail,
+	};
 }
